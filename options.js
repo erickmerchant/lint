2f88b518c813f89c0c905c91a98bf4d6ae6dadd1
Erick Merchant <erickmerchant@gmail.com>
2018-10-18 22:36:16 -0700

    Add more

--- options.js
+++ options.js
@@ -6,7 +6,7 @@ module.exports = {
   // homepage, version and bugs pulled from package.json
   version: pkg.version,
   homepage: pkg.homepage,
-  bugs: pkg.bugs ? pkg.bugs.url : '',
+  bugs: pkg.bugs.url,
   eslint: eslint, // pass any version of eslint >= 1.0.0
   cmd: 'lint', // should match the "bin" key in your package.json
   tagline: 'Standard plus some more', // displayed in output --help
