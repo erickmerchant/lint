ccce1206a348760b286192aed289f6d91e7cab39
Erick Merchant <erickmerchant@gmail.com>
2018-12-22 12:15:28 -0800

    Add eslint rules for possible errors

--- index.js
+++ index.js
@@ -45,6 +45,43 @@ module.exports = (deps) => {
         indent: ['error', 2, {
           SwitchCase: 1
         }],
+        // possible errors
+        // https://eslint.org/docs/rules/no-console
+        // https://eslint.org/docs/rules/no-extra-parens
+        // https://eslint.org/docs/rules/no-extra-semi
+        // https://eslint.org/docs/rules/no-func-assign
+        // https://eslint.org/docs/rules/no-inner-declarations
+        // https://eslint.org/docs/rules/no-obj-calls
+        // https://eslint.org/docs/rules/no-prototype-builtins
+        'for-direction': 'error',
+        'getter-return': 'error',
+        'no-async-promise-executor': 'error',
+        'no-await-in-loop': 'error',
+        'no-compare-neg-zero': 'error',
+        'no-cond-assign': 'error',
+        'no-constant-condition': 'error',
+        'no-control-regex': 'error',
+        'no-debugger': 'error',
+        'no-dupe-args': 'error',
+        'no-dupe-keys': 'error',
+        'no-duplicate-case': 'error',
+        'no-empty-character-class': 'error',
+        'no-empty': 'error',
+        'no-ex-assign': 'error',
+        'no-extra-boolean-cast': 'error',
+        'no-invalid-regexp': 'error',
+        'no-irregular-whitespace': 'error',
+        'no-misleading-character-class': 'error',
+        'no-regex-spaces': 'error',
+        'no-sparse-arrays': 'error',
+        'no-template-curly-in-string': 'error',
+        'no-unexpected-multiline': 'error',
+        'no-unreachable': 'error',
+        'no-unsafe-finally': 'error',
+        'no-unsafe-negation': 'error',
+        'require-atomic-updates': 'error',
+        'use-isnan': 'error',
+        'valid-typeof': 'error',
         // 'no-irregular-whitespace': ['error'],
         // 'no-multi-spaces': ['error'],
         // 'brace-style': ['error', '1tbs', {
