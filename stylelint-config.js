e959adb31a1d89be5aeddea4866b836a8495a981
Erick Merchant <erickmerchant@gmail.com>
2019-01-21 18:45:21 -0800

    Add configs

---
+++ stylelint-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'font-family-name-quotes': 'always-where-recommended',
+    'function-url-quotes': 'always',
+    'selector-attribute-quotes': 'always',
+    'string-quotes': 'single',
+    'at-rule-no-vendor-prefix': true,
+    'media-feature-name-no-vendor-prefix': true,
+    'property-no-vendor-prefix': true,
+    'selector-no-vendor-prefix': true,
+    'value-no-vendor-prefix': true,
+    indentation: 2
+  }
+}
