5a8f1e44abbce9b319d7a872465b9aa35ba2161c
Erick Merchant <erickmerchant@gmail.com>
2019-12-15 10:56:11 -0500

    Allow duplication again

--- stylelint-config.js
+++ stylelint-config.js
@@ -1,14 +1,8 @@
 require('stylelint-config-standard')
 
-const path = require('path')
-
 module.exports = {
   extends: 'stylelint-config-standard',
-  plugins: [
-    path.join(__dirname, 'plugins/no-duplicate-declarations.js')
-  ],
   rules: {
-    '@erickmerchant/no-duplicate-declations': true,
     'font-family-name-quotes': 'always-where-recommended',
     'function-url-quotes': 'always',
     'selector-attribute-quotes': 'always',
