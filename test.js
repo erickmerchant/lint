50353a2698f497f67aeacd32a633d587570e19e5
Erick Merchant <erickmerchant@gmail.com>
2020-05-08 19:01:09 -0400

    Dependencies

--- test.js
+++ test.js
@@ -1,21 +1,7 @@
 const test = require('tape')
 
-const execa = require('execa')
-
-test('', async (t) => {
+test('noop', async (t) => {
   t.plan(1)
 
   t.ok(true)
-})
-
-test('cli.js', async (t) => {
-  t.plan(2)
-
-  try {
-    await execa('node', ['./cli.js', '-h'])
-  } catch (e) {
-    t.ok(e)
-
-    t.equal(e.stdout.includes('Usage'), true)
-  }
 })
